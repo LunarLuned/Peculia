e7487c80284c2cf47f83f99ece44efbcb84d4b8d
LunarLuned <100322289+LunarLuned@users.noreply.github.com>
2022-09-06 21:17:20 -0700

    polished soul cloak texture

--- src/main/java/net/lunarluned/peculia/item/custom/HealingTomeItem.java
+++ src/main/java/net/lunarluned/peculia/item/custom/HealingTomeItem.java
@@ -1,11 +1,13 @@
 package net.lunarluned.peculia.item.custom;
 
 import net.lunarluned.peculia.sound.ModSoundEvents;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
@@ -22,7 +24,7 @@ public class HealingTomeItem extends Item {
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
 
 
-        ItemStack itemStack = user.getOffHandStack();
+        ItemStack itemStack = user.getActiveItem();
             user.addStatusEffect(new StatusEffectInstance(StatusEffects.INSTANT_HEALTH, 1, 0));
             user.getItemCooldownManager().set(this, 200);
             world.playSound(null, user.getPos().x, user.getPos().y, user.getPos().z, ModSoundEvents.EFFECT_ANCHORED_TELEPORT_FAIL, SoundCategory.NEUTRAL, 1f, 1f);
