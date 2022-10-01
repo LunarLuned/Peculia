54eec23731468741ba6bba3da0a8050dbc6eaed5
LunarLuned <100322289+LunarLuned@users.noreply.github.com>
2022-09-30 20:41:15 -0700

    fixed gravestone

--- src/main/java/net/lunarluned/peculia/block/custom/GravestoneBlock.java
+++ src/main/java/net/lunarluned/peculia/block/custom/GravestoneBlock.java
@@ -19,30 +19,45 @@ import org.jetbrains.annotations.NotNull;
 
 public class GravestoneBlock extends Block {
     public static final DirectionProperty FACING;
-    private static final VoxelShape SHAPE = Shapes.or(Block.box(2.0D, 0.0D, 1.0D, 14.0D, 14.0D, 14.0D));
+    public static final VoxelShape SHAPE_WEST;
+    public static final VoxelShape SHAPE_NORTH;
+    public static final VoxelShape SHAPE_EAST;
+    public static final VoxelShape SHAPE_SOUTH;
+    private static final VoxelShape SHAPE = Shapes.or(Block.box(6.0D, 0.0D, 2.0D, 10.0D, 14.0D, 14.0D));
 
     public GravestoneBlock(Properties properties) {
         super(properties);
-    }
-
-    public VoxelShape getShape(@NotNull BlockState state, @NotNull BlockGetter world, @NotNull BlockPos pos, @NotNull CollisionContext context) {
-        return SHAPE;
     }
 
     public BlockState getStateForPlacement(BlockPlaceContext ctx) {
         return this.defaultBlockState().setValue(FACING, ctx.getHorizontalDirection().getClockWise());
     }
 
-    public BlockState rotate(BlockState state, Rotation rotation) {
-        return state.setValue(FACING, rotation.rotate(state.getValue(FACING)));
-    }
-
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
         builder.add(new Property[]{FACING});
     }
 
+    public VoxelShape getShape(BlockState blockState, BlockGetter blockGetter, BlockPos blockPos, CollisionContext collisionContext) {
+        switch (blockState.getValue(FACING)) {
+            case NORTH:
+                return SHAPE_NORTH;
+            case SOUTH:
+                return SHAPE_SOUTH;
+            case EAST:
+                return SHAPE_EAST;
+            case WEST:
+                return SHAPE_WEST;
+            default:
+                return SHAPE;
+        }
+    }
+
     static {
         FACING = HorizontalDirectionalBlock.FACING;
+        SHAPE_EAST = Shapes.or(Block.box(2.0D, 0.0D, 6.0D, 14.0D, 14.0D, 10.0D));
+        SHAPE_WEST = Shapes.or(Block.box(2.0D, 0.0D, 6.0D, 14.0D, 14.0D, 10.0D));
+        SHAPE_NORTH = Shapes.or(Block.box(6.0D, 0.0D, 2.0D, 10.0D, 14.0D, 14.0D));
+        SHAPE_SOUTH = Shapes.or(Block.box(6.0D, 0.0D, 2.0D, 10.0D, 14.0D, 14.0D));
     }
 
 }
