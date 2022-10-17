122ffc2f94241f5a77b0ad2bdaef664767a40405
LunarLuned <100322289+LunarLuned@users.noreply.github.com>
2022-10-16 21:47:58 -0700

    added new Ghoul Mob Type

---
+++ src/main/java/net/lunarluned/peculia/common/registry/ModMobTypes.java
@@ -0,0 +1,7 @@
+package net.lunarluned.peculia.common.registry;
+
+import net.minecraft.world.entity.MobType;
+
+public class ModMobTypes extends MobType {
+    public static final MobType GHOUL = new MobType();
+}
