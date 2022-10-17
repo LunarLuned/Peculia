7fd2b0539d62d986d3f4829805193547795bdafa
LunarLuned <100322289+LunarLuned@users.noreply.github.com>
2022-10-16 21:47:36 -0700

    added Ghost Mob (pt2)

--- src/main/java/net/lunarluned/peculia/common/registry/entity/registry/ModEntities.java
+++ src/main/java/net/lunarluned/peculia/common/registry/entity/registry/ModEntities.java
@@ -3,6 +3,7 @@ package net.lunarluned.peculia.common.registry.entity.registry;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
 import net.lunarluned.peculia.Peculia;
 import net.lunarluned.peculia.common.registry.entity.living_entities.coal_geode.CoalGeodeEntity;
+import net.lunarluned.peculia.common.registry.entity.living_entities.ghost.GhostEntity;
 import net.lunarluned.peculia.common.registry.entity.projectiles.SwordProjectionEntity;
 import net.lunarluned.peculia.misc.CoreRegistry;
 import net.minecraft.core.Registry;
@@ -16,6 +17,7 @@ public class ModEntities {
 
     public static final EntityType<SwordProjectionEntity> SWORD_PROJECTION = register("sword_projection", EntityType.Builder.of(SwordProjectionEntity::new, MobCategory.MISC).sized(1, 1).clientTrackingRange(10));
     public static final EntityType<CoalGeodeEntity> COAL_GEODE = register("coal_geode", EntityType.Builder.of(CoalGeodeEntity::new, MobCategory.CREATURE).sized(0.7f, 0.85f).clientTrackingRange(10));
+    public static final EntityType<GhostEntity> GHOST = register("ghost", EntityType.Builder.of(GhostEntity::new, MobCategory.MONSTER).sized(0.8f, 1.9f).clientTrackingRange(10));
 
     public static <T extends Entity> EntityType<T> register(String id, EntityType.Builder<T> type) {
         return ENTITIES.register(id, type.build(id));
@@ -23,6 +25,7 @@ public class ModEntities {
 
     public static void registerAttributes() {
         FabricDefaultAttributeRegistry.register(COAL_GEODE, CoalGeodeEntity.createCoalGeodeAttributes());
+        FabricDefaultAttributeRegistry.register(GHOST, GhostEntity.createGhostAttributes());
     }
 
     public static void registerModEntities() {
