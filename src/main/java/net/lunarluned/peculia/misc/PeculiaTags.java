df2b4d2dff6502e7848166092e168fc3f3f1c46c
Sydney <94016034+Sydokiddo@users.noreply.github.com>
2022-10-23 23:53:37 -0700

    Tags and such lol

--- src/main/java/net/lunarluned/peculia/misc/PeculiaTags.java
+++ src/main/java/net/lunarluned/peculia/misc/PeculiaTags.java
@@ -7,11 +7,14 @@ import net.minecraft.tags.TagKey;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.item.Instrument;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
 
 public class PeculiaTags {
 
     public static final TagKey<Item> ALERTING_ITEMS = TagKey.create(Registry.ITEM_REGISTRY, new ResourceLocation("peculia", "alerting_items"));
     public static final TagKey<Instrument> TUSK_HORNS = TagKey.create(Registry.INSTRUMENT_REGISTRY, new ResourceLocation("peculia", "tusk_horns"));
     public static final TagKey<EntityType<?>> GHOST_CANNOT_ATTACK = TagKey.create(Registry.ENTITY_TYPE_REGISTRY, new ResourceLocation("peculia", "ghost_cannot_attack"));
+    public static final TagKey<Block> GRIM_WOOD_LOGS = TagKey.create(Registry.BLOCK_REGISTRY, new ResourceLocation("peculia", "grim_wood_logs"));
+    public static final TagKey<Item> GRIM_WOOD_LOGS_ITEM = TagKey.create(Registry.ITEM_REGISTRY, new ResourceLocation("peculia", "grim_wood_logs"));
 
 }
