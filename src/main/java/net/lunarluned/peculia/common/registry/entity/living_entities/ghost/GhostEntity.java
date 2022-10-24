8c35ec15413ffb9b2c0d11208aa91ecb880b1ea3
Sydney <94016034+Sydokiddo@users.noreply.github.com>
2022-10-23 23:07:27 -0700

    Ghost tweaks

--- src/main/java/net/lunarluned/peculia/common/registry/entity/living_entities/ghost/GhostEntity.java
+++ src/main/java/net/lunarluned/peculia/common/registry/entity/living_entities/ghost/GhostEntity.java
@@ -1,9 +1,8 @@
 package net.lunarluned.peculia.common.registry.entity.living_entities.ghost;
 
 import net.lunarluned.peculia.common.registry.ModMobTypes;
-import net.lunarluned.peculia.common.registry.entity.registry.ModEntities;
 import net.lunarluned.peculia.effect.ModEffects;
-import net.lunarluned.peculia.item.modmats.ModArmorMaterials;
+import net.lunarluned.peculia.misc.PeculiaTags;
 import net.lunarluned.peculia.sound.ModSoundEvents;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.nbt.CompoundTag;
@@ -15,35 +14,23 @@ import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.*;
-import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.control.LookControl;
 import net.minecraft.world.entity.ai.goal.*;
 import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
-import net.minecraft.world.entity.ai.sensing.Sensor;
-import net.minecraft.world.entity.ai.sensing.SensorType;
 import net.minecraft.world.entity.monster.*;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ArmorItem;
-import net.minecraft.world.item.ArmorMaterials;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.pathfinder.BlockPathTypes;
-import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
-import org.checkerframework.checker.units.qual.A;
 import org.jetbrains.annotations.NotNull;
-import org.spongepowered.include.com.google.common.collect.ImmutableList;
-
-import java.util.Iterator;
 
 public class GhostEntity extends Monster {
+
     private static final EntityDataAccessor<Boolean> ANGERED = SynchedEntityData.defineId(GhostEntity.class, EntityDataSerializers.BOOLEAN);
 
-
+    // Placeholder goals, will eventually be replaced with a brain
 
     public void registerGoals() {
         this.goalSelector.addGoal(3, new RandomStrollGoal(this, 0.9));
@@ -54,11 +41,11 @@ public class GhostEntity extends Monster {
     protected void addBehaviourGoals() {
         this.goalSelector.addGoal(2, new MeleeAttackGoal(this, 1.0, false));
         this.goalSelector.addGoal(7, new WaterAvoidingRandomStrollGoal(this, 1.0));
-        this.targetSelector.addGoal(2, new NearestAttackableTargetGoal(this, LivingEntity.class, true));
+        this.targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(this, LivingEntity.class, true));
     }
 
-    public boolean canAttackType(EntityType<?> entityType) {
-        return entityType != ModEntities.GHOST && entityType != ModEntities.WISP && entityType != EntityType.IRON_GOLEM && entityType != EntityType.ZOMBIE && entityType != EntityType.ZOMBIFIED_PIGLIN && entityType != EntityType.SKELETON && entityType != EntityType.CREEPER && super.canAttackType(entityType);
+    public boolean canAttackType(@NotNull EntityType<?> entityType) {
+        return (!entityType.is(PeculiaTags.GHOST_CANNOT_ATTACK));
     }
 
     public void handleEntityEvent(byte b) {
@@ -81,11 +68,13 @@ public class GhostEntity extends Monster {
         return true;
     }
 
+    // Ghost Sounds
+
     public SoundEvent getAmbientSound() {
         return ModSoundEvents.ENTITY_GHOST_MOAN;
     }
 
-    public SoundEvent getHurtSound(DamageSource damageSource) {
+    public SoundEvent getHurtSound(@NotNull DamageSource damageSource) {
         return ModSoundEvents.ENTITY_GHOST_HURT;
     }
 
@@ -97,54 +86,63 @@ public class GhostEntity extends Monster {
         return (float)this.getAttributeValue(Attributes.ATTACK_DAMAGE);
     }
 
+    // On Hurt Method
+
     public boolean doHurtTarget(Entity entity) {
+
         this.attackAnimationState.start(this.tickCount);
         this.level.broadcastEntityEvent(this, (byte)4);
         float f = this.getAttackDamage();
         float g = (int)f > 0 ? f / 2.0F + (float)this.random.nextInt((int)f) : f;
         boolean bl = entity.hurt(DamageSource.mobAttack(this), g);
-        if (bl) {
-            double var10000;
-            if (entity instanceof LivingEntity) {
-                LivingEntity livingEntity = (LivingEntity)entity;
-                var10000 = livingEntity.getAttributeValue(Attributes.KNOCKBACK_RESISTANCE);
-            } else {
-                var10000 = 0.0;
-            }
 
-            double d = var10000;
-            double e = Math.max(0.0, 1.0 - d);
+        if (bl) {
+            if (entity instanceof LivingEntity livingEntity) {
+                livingEntity.getAttributeValue(Attributes.KNOCKBACK_RESISTANCE);
+            }
             this.doEnchantDamageEffects(this, entity);
         }
+
+        // If the Ghost is angered, it will heal itself upon attacking
+
         if ((this.getHealth() < this.getMaxHealth() / 2) || this.isAngered()) {
             if (this.getRandom().nextInt(10) <= 10) {
                 this.addEffect(new MobEffectInstance(MobEffects.HEAL, 1, 0, false, false, false));
             }
         }
+
+        // Ghosts add the Cursed Effect to their target upon attacking
+
+        assert entity instanceof LivingEntity;
         ((LivingEntity) entity).addEffect(new MobEffectInstance(ModEffects.CURSED, 80, 0, false, true, true));
 
-        //this is the attack sound. its wrong rn but
         this.playSound(ModSoundEvents.ENTITY_GHOST_JUMPSCARE, 1.0F, 1.0F);
+
         return bl;
     }
 
-
-
-    protected static final ImmutableList<SensorType<? extends Sensor<? super GhostEntity>>> SENSOR_TYPES =
-            ImmutableList.of(SensorType.NEAREST_LIVING_ENTITIES, SensorType.NEAREST_PLAYERS);
-
-
     public GhostEntity(EntityType<? extends Monster> entityType, Level level) {
         super(entityType, level);
+
         this.lookControl = new LookControl(this);
+
         this.setPathfindingMalus(BlockPathTypes.DANGER_FIRE, 16.0f);
         this.setPathfindingMalus(BlockPathTypes.DAMAGE_FIRE, -1.0f);
         this.setPathfindingMalus(BlockPathTypes.WATER, -1.0f);
         this.setPathfindingMalus(BlockPathTypes.WATER_BORDER, 16.0f);
+        this.setPathfindingMalus(BlockPathTypes.LAVA, -1.0f);
         this.setPathfindingMalus(BlockPathTypes.COCOA, -1.0f);
         this.setPathfindingMalus(BlockPathTypes.FENCE, -1.0f);
         this.setPathfindingMalus(BlockPathTypes.TRAPDOOR, -1.0f);
+        this.setPathfindingMalus(BlockPathTypes.POWDER_SNOW, -1.0f);
+        this.setPathfindingMalus(BlockPathTypes.DANGER_POWDER_SNOW, -1.0f);
+        this.setPathfindingMalus(BlockPathTypes.DANGER_CACTUS, 8.0f);
+        this.setPathfindingMalus(BlockPathTypes.DAMAGE_CACTUS, -1.0f);
+        this.setPathfindingMalus(BlockPathTypes.DANGER_OTHER, -1.0f);
+        this.setPathfindingMalus(BlockPathTypes.DAMAGE_OTHER, -1.0f);
     }
+
+    // Ghost Animation States
 
     public final AnimationState idleAnimationState = new AnimationState();
     public final AnimationState fallAnimationState = new AnimationState();
@@ -153,6 +151,9 @@ public class GhostEntity extends Monster {
     public final AnimationState attackAnimationState = new AnimationState();
 
     public void aiStep() {
+
+        // If the Ghost is exposed to sunlight, it will become angered
+
         if (this.isAlive()) {
             boolean bl = this.isSunSensitive() && this.isSunBurnTick() && !this.isAngered();
             if (bl) {
@@ -160,26 +161,32 @@ public class GhostEntity extends Monster {
             }
             updateAngered(level);
         }
+
+        // The Ghost emits ambient Soul Fire Flame particles
+
         if (this.level.isClientSide) {
             for(int i = 0; i < 2; ++i) {
-                Vec3 vec3 = this.getDeltaMovement();
-                this.level.addParticle(ParticleTypes.SOUL_FIRE_FLAME, this.getX() + (this.random.nextDouble() - 0.5) * (double)this.getBbWidth(), this.getY() + 0.4, this.getZ() + (this.random.nextDouble() - 0.5) * (double)this.getBbWidth(), vec3.x * -0.2, 0.1, vec3.z * -0.2);
+                if (this.tickCount % 5 == 0) {
+                    Vec3 vec3 = this.getDeltaMovement();
+                    this.level.addParticle(ParticleTypes.SOUL_FIRE_FLAME, this.getX() + (this.random.nextDouble() - 0.5) * (double) this.getBbWidth(), this.getY() + 0.4, this.getZ() + (this.random.nextDouble() - 0.5) * (double) this.getBbWidth(), vec3.x * -0.2, 0.1, vec3.z * -0.2);
+                }
             }
         }
+
         super.aiStep();
     }
 
-    //attributes
+    // Attributes
 
     public static AttributeSupplier.Builder createGhostAttributes() {
         return createMobAttributes().add(Attributes.MAX_HEALTH, 30.0).add(Attributes.MOVEMENT_SPEED, 0.25f).add(Attributes.ARMOR, 8.0).add(Attributes.KNOCKBACK_RESISTANCE, 0.4).add(Attributes.ATTACK_DAMAGE, 8.0).add(Attributes.FOLLOW_RANGE, 8.0);
     }
 
+    // Ghosts are part of the Ghoul mob type
 
     public MobType getMobType() {
         return ModMobTypes.GHOUL;
     }
-
 
     public boolean isAngered() {
         return this.entityData.get(ANGERED);
@@ -195,8 +202,9 @@ public class GhostEntity extends Monster {
 
     @Override
     public boolean canBeLeashed(@NotNull Player player) {
-        return !this.isLeashed();
+        return false;
     }
+
     @Override
     protected void defineSynchedData() {
         this.entityData.define(ANGERED, false);
@@ -204,20 +212,19 @@ public class GhostEntity extends Monster {
     }
 
     @Override
-    public void addAdditionalSaveData(CompoundTag nbt) {
+    public void addAdditionalSaveData(@NotNull CompoundTag nbt) {
         super.addAdditionalSaveData(nbt);
         nbt.putBoolean("IsAngered", this.isAngered());
     }
 
     private void updateAngered(Level level) {
         if (!level.isClientSide()) {
-            LivingEntity livingEntity = this.getTarget();
+            this.getTarget();
         }
-
     }
 
     @Override
-    public void readAdditionalSaveData(CompoundTag nbt) {
+    public void readAdditionalSaveData(@NotNull CompoundTag nbt) {
         super.readAdditionalSaveData(nbt);
         this.setAngered(nbt.getBoolean("IsAngered"));
     }
@@ -228,12 +235,11 @@ public class GhostEntity extends Monster {
         return i;
     }
 
-
-
-    // Plays the animations upon ticking
-
     @Override
     public void tick() {
+
+        // Plays the falling animation and gives the Ghost Slow Falling if falling while not angered
+
         if (!this.isOnGround() && !this.isAngered()) {
             this.attackAnimationState.stop();
             this.walkAnimationState.stop();
@@ -241,35 +247,37 @@ public class GhostEntity extends Monster {
             this.fallAnimationState.startIfStopped(this.tickCount);
             this.addEffect(new MobEffectInstance(MobEffects.SLOW_FALLING, 30, 0, false, false, false));
         }
+
+        // Adds a movement and damage increase and spawns Soul particles when the Ghost is angered
+
         if (this.isAngered()) {
             this.spawnSoulSpeedParticle();
             this.addEffect(new MobEffectInstance(MobEffects.DAMAGE_BOOST, 30, 0, false, false, false));
             this.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 30, 1, false, false, false));
         }
-        LivingEntity livingEntity = this.getTarget();
+
+        // If the Ghost is at half health, it will become empowered and recieve the Determined effect
+
         if (this.getHealth() < this.getMaxHealth() / 2) {
             if (this.getRandom().nextInt(1000) >= 1000) {
                 this.playSound(ModSoundEvents.ENTITY_GHOST_EMPOWERED, 1.0F, 1.0F);
             }
-
-                this.addEffect(new MobEffectInstance(ModEffects.DETERMINED, 30, 3, false, false, false));
-
+            this.addEffect(new MobEffectInstance(ModEffects.DETERMINED, 30, 3, false, false, false));
         }
 
-            if (this.isMoving()) {
-                this.idleAnimationState.stop();
-                this.fallAnimationState.stop();
-                this.walkAnimationState.startIfStopped(this.tickCount);
-            } else {
-                this.walkAnimationState.stop();
-                this.fallAnimationState.stop();
-                this.idleAnimationState.startIfStopped(this.tickCount);
-            }
-        super.tick();
-    }
+        // Plays the animations upon ticking
 
-    private boolean isInPose(Pose pose) {
-        return this.getPose() == pose;
+        if (this.isMoving()) {
+            this.idleAnimationState.stop();
+            this.fallAnimationState.stop();
+            this.walkAnimationState.startIfStopped(this.tickCount);
+        } else {
+            this.walkAnimationState.stop();
+            this.fallAnimationState.stop();
+            this.idleAnimationState.startIfStopped(this.tickCount);
+        }
+
+        super.tick();
     }
 
     @Override
@@ -282,9 +290,11 @@ public class GhostEntity extends Monster {
         return 20;
     }
 
-    public boolean isPreventingPlayerRest(Player player) {
+    public boolean isPreventingPlayerRest(@NotNull Player player) {
         return true;
     }
+
+    // Ghosts despawn when in Peaceful difficulty
 
     protected boolean shouldDespawnInPeaceful() {
         return true;
--- src/main/java/net/lunarluned/peculia/misc/PeculiaTags.java
+++ src/main/java/net/lunarluned/peculia/misc/PeculiaTags.java
@@ -2,14 +2,16 @@ package net.lunarluned.peculia.misc;
 
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.EntityTypeTags;
 import net.minecraft.tags.TagKey;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.item.Instrument;
 import net.minecraft.world.item.Item;
 
 public class PeculiaTags {
 
     public static final TagKey<Item> ALERTING_ITEMS = TagKey.create(Registry.ITEM_REGISTRY, new ResourceLocation("peculia", "alerting_items"));
-
     public static final TagKey<Instrument> TUSK_HORNS = TagKey.create(Registry.INSTRUMENT_REGISTRY, new ResourceLocation("peculia", "tusk_horns"));
+    public static final TagKey<EntityType<?>> GHOST_CANNOT_ATTACK = TagKey.create(Registry.ENTITY_TYPE_REGISTRY, new ResourceLocation("peculia", "ghost_cannot_attack"));
 
 }
