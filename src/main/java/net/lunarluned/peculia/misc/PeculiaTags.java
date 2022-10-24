9ae9a028384423407a41ec23ca38ecc3734790be
LunarLuned <100322289+LunarLuned@users.noreply.github.com>
2022-10-24 00:55:52 -0700

    made a banishable mobs tag. will do more with it later

--- src/main/java/net/lunarluned/peculia/misc/PeculiaTags.java
+++ src/main/java/net/lunarluned/peculia/misc/PeculiaTags.java
@@ -13,6 +13,7 @@ public class PeculiaTags {
 
     public static final TagKey<Item> ALERTING_ITEMS = TagKey.create(Registry.ITEM_REGISTRY, new ResourceLocation("peculia", "alerting_items"));
     public static final TagKey<Instrument> TUSK_HORNS = TagKey.create(Registry.INSTRUMENT_REGISTRY, new ResourceLocation("peculia", "tusk_horns"));
+    public static final TagKey<EntityType<?>> BANISHABLE_MOBS = TagKey.create(Registry.ENTITY_TYPE_REGISTRY, new ResourceLocation("peculia", "banishable_mobs"));
     public static final TagKey<EntityType<?>> GHOST_CANNOT_ATTACK = TagKey.create(Registry.ENTITY_TYPE_REGISTRY, new ResourceLocation("peculia", "ghost_cannot_attack"));
     public static final TagKey<Block> GRIM_WOOD_LOGS = TagKey.create(Registry.BLOCK_REGISTRY, new ResourceLocation("peculia", "grim_wood_logs"));
     public static final TagKey<Item> GRIM_WOOD_LOGS_ITEM = TagKey.create(Registry.ITEM_REGISTRY, new ResourceLocation("peculia", "grim_wood_logs"));
